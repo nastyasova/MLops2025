36fcfb7ee8728764eaddcf6982c4e2d503e4e278
nastyasova <100161119+nastyasova@users.noreply.github.com>
2025-11-05 02:54:56 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,10 +1,29 @@
+from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments
 from src.data import load_and_preprocess_data
-import yaml
+from configs.config import load_config
 
-with open("configs/train_config.yaml") as f:
-    config = yaml.safe_load(f)
+config = load_config()
+train_ds, test_ds, tokenizer = load_and_preprocess_data(config)
 
-train_ds, test_ds, tok = load_and_preprocess_data(config)
-print(len(train_ds), len(test_ds))
-print(tok("I love this movie!"))
+model = AutoModelForSequenceClassification.from_pretrained(
+    config["model"]["name"], num_labels=2
+)
 
+training_args = TrainingArguments(
+    output_dir="./models/checkpoints",
+    evaluation_strategy="epoch",
+    per_device_train_batch_size=8,
+    num_train_epochs=1,
+    logging_dir="./logs",
+    logging_steps=10
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_ds,
+    eval_dataset=test_ds,
+    tokenizer=tokenizer,
+)
+
+trainer.train()
