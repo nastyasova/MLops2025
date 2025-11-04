51e7762ea3f2de87fa49e27c8e4a93d6106b340a
nastyasova <100161119+nastyasova@users.noreply.github.com>
2025-11-05 01:11:11 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,10 @@
+from src.data import load_and_preprocess_data
+import yaml
+
+with open("configs/train_config.yaml") as f:
+    config = yaml.safe_load(f)
+
+train_ds, test_ds, tok = load_and_preprocess_data(config)
+print(len(train_ds), len(test_ds))
+print(tok("I love this movie!"))
 
