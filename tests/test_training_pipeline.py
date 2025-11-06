007a49f3a584f56d01eed2c252b26cda2ed63925
nastyasova <100161119+nastyasova@users.noreply.github.com>
2025-11-06 03:29:16 +0300

    Update test_training_pipeline.py

--- tests/test_training_pipeline.py
+++ tests/test_training_pipeline.py
@@ -70,12 +70,12 @@ def test_training_and_saving(small_training_setup):
     assert found, f"Файл весов не найден ни в одном из форматов: {weight_files}"
     
     for file in expected_files:
-    path = os.path.join(config["output_dir"], file)
-    if file == "trainer_state.json":
-        if not os.path.exists(path):
-            print(f"[INFO] Пропускаем проверку: {file} не был создан (это допустимо)")
-            continue
-    assert os.path.exists(path), f"Файл не найден: {path}"
+        path = os.path.join(config["output_dir"], file)
+        if file == "trainer_state.json":
+            if not os.path.exists(path):
+                print(f"[INFO] Пропускаем проверку: {file} не был создан (это допустимо)")
+                continue
+        assert os.path.exists(path), f"Файл не найден: {path}"
 
 
 
