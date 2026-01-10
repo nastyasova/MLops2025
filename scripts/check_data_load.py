ce29261c515e89b7605e992aa4f01d99278ee2af
nastyasova <100161119+nastyasova@users.noreply.github.com>
2026-01-11 01:30:06 +0300

    Create check_data_load.py

---
+++ scripts/check_data_load.py
@@ -0,0 +1,11 @@
+from src.data import load_and_preprocess_data
+import yaml
+
+if __name__ == "__main__":
+    with open("configs/train_config.yaml") as f:
+        config = yaml.safe_load(f)
+
+    train_ds, test_ds, tokenizer = load_and_preprocess_data(config)
+    print(f"Train size: {len(train_ds)}, Test size: {len(test_ds)}")
+    example = tokenizer("I love this movie!")["input_ids"][:10]
+    print("Пример токенов:", example)
